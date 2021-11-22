d349cd55e9c17468e6f3e4441f9fe4bab31a1d68
okuoku <mjt@cltn.org>
2021-11-22 20:58:02 +0900

    amigo_powercfg: Adjust PLL setting

--- amigo_powercfg.c
+++ amigo_powercfg.c
@@ -2,6 +2,11 @@
 
 void
 powercfg_boot(void){
+    // From Maixduino
+    sysctl_pll_set_freq(SYSCTL_PLL0, 800000000UL);
+    sysctl_pll_set_freq(SYSCTL_PLL1, 300000000UL);
+    sysctl_pll_set_freq(SYSCTL_PLL2, 45158400UL);
+
     // LCD digital I/O pins
     sysctl_set_power_mode(SYSCTL_POWER_BANK6, SYSCTL_POWER_V18);
     // DVP(Camera)
